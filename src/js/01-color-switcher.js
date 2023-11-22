2c52e2f7da40c57788de9079451863b29fa543c9
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-22 20:49:28 +0100

    task1 - completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start');
+const stopButton = document.querySelector('[data-stop');
 
+let allowColorChange = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  allowColorChange = true;
+  startButton.setAttribute('disabled', '');
+  stopButton.removeAttribute('disabled');
+});
+
+stopButton.addEventListener('click', () => {
+  allowColorChange = false;
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', '');
+});
+
+setInterval(() => {
+  if (allowColorChange) {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+}, 1000);
