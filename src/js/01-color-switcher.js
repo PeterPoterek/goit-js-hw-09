e37492b83b55003815ee2040ad81c68c158f548e
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-23 18:33:09 +0100

    task1 - refactor

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,7 @@ const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
 let allowColorChange = false;
+let colorSwitcher;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -11,19 +12,19 @@ const startColorChange = () => {
   allowColorChange = true;
   startButton.setAttribute('disabled', '');
   stopButton.removeAttribute('disabled');
+
+  colorSwitcher = setInterval(() => {
+    if (allowColorChange) {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }
+  }, 1000);
 };
 const stopColorChange = () => {
   allowColorChange = false;
   startButton.removeAttribute('disabled');
   stopButton.setAttribute('disabled', '');
+  clearInterval(colorSwitcher);
 };
 
 startButton.addEventListener('click', startColorChange);
-
 stopButton.addEventListener('click', stopColorChange);
-
-const colorSwitcher = setInterval(() => {
-  if (allowColorChange) {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }
-}, 1000);
