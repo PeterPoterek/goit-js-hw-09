0945d71b7bb49915034d3f276179fe7d243de8d9
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-23 18:56:25 +0100

    promises resolve/reject logic

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
-    // Fulfill
+    return new Promise((resolve, reject) => {
+      resolve({ position, delay });
+    });
   } else {
-    // Reject
+    return new Promise((resolve, reject) => {
+      reject({ position, delay });
+    });
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
